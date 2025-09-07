0e930d898ecd39f23b053d4ab7bbc72ad0df0f61
Rafael <132296553+R4f4ell@users.noreply.github.com>
2025-09-06 23:06:51 -0300

    feat: adicionando onda fixa no rodapé e aplicando carregamento eager para imagens principais (logo e fundo) na página de início

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/images/inicio/logoFavicon.png";
+import imgFundo from "../../assets/images/inicio/imgInicial-mobile.webp";
 import "./inicio.scss";
 
 const Inicio = () => {
@@ -7,10 +8,43 @@ const Inicio = () => {
 
   return (
     <main className="inicio">
-      <div className="inicio__background"></div>
+      {/* Fundo escuro + onda SVG */}
+      <div className="inicio__background">
+        {/* Imagem de fundo */}
+        <img
+          src={imgFundo}
+          alt=""
+          className="inicio__bg-img"
+          loading="eager"
+        />
+
+        {/* Onda */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          preserveAspectRatio="none"
+        >
+          <defs>
+            <linearGradient id="waveGrad" x1="0" x2="0" y1="0" y2="1">
+              <stop offset="0%" stopColor="#9DB610" />
+              <stop offset="100%" stopColor="#0a0a0a" />
+            </linearGradient>
+          </defs>
+
+          <path
+            fill="url(#waveGrad)"
+            d="M0,224L40,213.3C80,203,160,181,240,181.3C320,181,400,203,480,197.3C560,192,640,160,720,154.7C800,149,880,171,960,181.3C1040,192,1120,192,1200,176C1280,160,1360,128,1400,112L1440,96L1440,320L0,320Z"
+          />
+        </svg>
+      </div>
 
       <header className="inicio__header">
-        <img src={logo} alt="Logo do sistema" className="logo" />
+        <img
+          src={logo}
+          alt="Logo do sistema"
+          className="logo"
+          loading="eager"
+        />
       </header>
 
       <section className="inicio__content">
